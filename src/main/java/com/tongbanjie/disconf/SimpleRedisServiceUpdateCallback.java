3af55c4108b4ced8bbcd8b1f9b6f4757dee3ce55
子游 <ziyou@tongbanjie.com>
2016-11-02 16:18:43 +0800

    改成 @DisconfUpdateService(confFileKeys = "redis.properties")

--- src/main/java/com/tongbanjie/disconf/SimpleRedisServiceUpdateCallback.java
+++ src/main/java/com/tongbanjie/disconf/SimpleRedisServiceUpdateCallback.java
@@ -18,7 +18,7 @@ import com.baidu.disconf.client.common.update.IDisconfUpdate;
  */
 @Service
 @Scope("singleton")
-@DisconfUpdateService(classes = {JedisConfig.class})
+@DisconfUpdateService(confFileKeys = "redis.properties")
 public class SimpleRedisServiceUpdateCallback implements IDisconfUpdate {
 
     protected static final Logger LOGGER = LoggerFactory.getLogger(SimpleRedisServiceUpdateCallback.class);
